587305de4d50d2541d9de6e3dd01c58ab734c63e
Yuval Adam <yuv.adm@gmail.com>
2014-08-13 02:01:29 +0300

    Fix s1c1

--- set1/challenge1.py
+++ set1/challenge1.py
@@ -4,7 +4,6 @@ s = '49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7
 expected = 'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
 
 def hex_str_to_base64(s):
-    ba = bytearray([int(s[(i*2):(i*2)+2], 16) for i in range(len(s)/2)])
-    return base64.b64encode(ba)
+    return base64.b64encode(s.decode('hex'))
 
-assert hex_str_to_base64(s) == 'expected'
+assert hex_str_to_base64(s) == expected
